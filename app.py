0fcf8d7e824ac041814bccd1a55585543f272b26
nayeemnazmul <md.nazmul@northsouth.edu>
2019-12-30 11:13:23 +0600

    added GET values API

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 #!/bin/python3
 
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, request, jsonify
 import redis
 
 app = Flask(__name__)
 app.config['BUNDLE_ERRORS'] = True
 app.debug = True
 db = redis.Redis('localhost') #connect to server
-api = Api(app)
-
-ttl = 300 # 5 minute
+ttl = 300  # 5 minute
 
 
 @app.route('/')
@@ -18,17 +15,22 @@ def welcome():
     return "Welcome"
 
 
-class KeyValuesAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser(bundle_errors=True)
-        self.reqparse.add_argument(type=dict, location='json')
-        super(KeyValuesAPI, self).__init__()
+@app.route('/values', methods=['POST'])
+def save_values():
+    if request.method == 'POST':
+        response_data = request.json
 
-    def post(self, value_dict):
-        pass
-        
+        update_value_dict = {}
+        for key, value in response_data.items():
+            flag = db.set(key, value, ex=ttl, nx=True)
 
-api.add_resource(KeyValuesAPI, '/values', endpoint='values')
+            if flag is None:
+                update_value_dict.update({key: "Already exists"})
+
+        response_data.update(update_value_dict)
+
+    return jsonify(response_data), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
